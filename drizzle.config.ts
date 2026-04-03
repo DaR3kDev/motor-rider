3c2aab96ee3a096dc9ef5e5531f12b03daecea17
DaR3kDev <villegasdeveloper@outlook.es>
2026-04-02 21:47:06 -0500

    first commit

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import 'dotenv/config'
+import { defineConfig } from 'drizzle-kit'
+
+export default defineConfig({
+  schema: './src/shared/db/*.schema.ts',
+  out: './src/shared/db/migrations',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+})
